6755c2a764c04dd9b5e41a1b99d27b8e00d09a83
Sumant <sumantyadav3086@gmail.com>
2025-02-10 21:42:14 +0530

    tree

---
+++ K Sum Paths.cpp
@@ -0,0 +1,52 @@
+// K Sum Paths
+// Difficulty: MediumAccuracy: 44.73%Submissions: 114K+Points: 4
+// Given a binary tree and an integer k, determine the number of downward-only paths where the sum of the node values in the path equals k. A path can start and end at any node within the tree but must always move downward (from parent to child).
+
+// Examples:
+
+// Input: k = 7   
+
+// Output: 3
+// Explanation: The following paths sum to k 
+ 
+// Input: k = 3
+
+// Output: 2
+// Explanation:
+// Path 1 : 1 -> 2 (Sum = 3)
+// Path 2 : 3 (Sum = 3)
+
+
+// Constraints:
+
+// 1 ≤ number of nodes ≤ 104
+// -100 ≤ node value ≤ 100
+// -109 ≤ k ≤ 109
+// Company Tags
+// FlipkartAmazonWalmart
+// Topic Tags
+// Related Interview Experiences
+// Related Articles
+// Expected Complexities
+// If 
+
+int sumK(Node *root, int k) {
+    // code here
+     unordered_map<int,int> ump;
+    ump[0]=1;
+    solve(root, k, ump, 0);
+    return total;
+}
+private:
+int total = 0;
+const int mod=1e9+7;
+void solve(Node* node, int k, unordered_map<int,int> &ump, int sum){
+    if(node==nullptr) return;
+    sum+=node->data;
+    if(ump.find(sum-k)!=ump.end()) total=(total+ump[sum-k])%mod;
+    ump[sum]++;
+    solve(node->left, k, ump, sum);
+    solve(node->right, k, ump, sum);
+    ump[sum]--;
+    if(ump[sum]==0) ump.erase(sum);
+}
