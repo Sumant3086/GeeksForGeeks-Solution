25e11047fbd9abf41ea07131121fa054e8ff83d2
Sumant <sumantyadav3086@gmail.com>
2025-02-19 11:38:22 +0530

    with priority queue

---
+++ Merge K sorted linked lists.cpp
@@ -0,0 +1,66 @@
+// Merge K sorted linked lists
+// Difficulty: MediumAccuracy: 57.01%Submissions: 91K+Points: 4Average Time: 60m
+// Given an array arr[] of n sorted linked lists of different sizes. The task is to merge them in such a way that after merging they will be a single sorted linked list, then return the head of the merged linked list.
+
+// Examples:
+
+// Input: arr[] = [1 -> 2 -> 3, 4 -> 5, 5 -> 6, 7 -> 8]
+// Output: 1 -> 2 -> 3 -> 4 -> 5 -> 5 -> 6 -> 7 -> 8
+// Explanation:
+// The arr[] has 4 sorted linked list of size 3, 2, 2, 2.
+// 1st list: 1 -> 2-> 3
+// 2nd list: 4 -> 5
+// 3rd list: 5 -> 6
+// 4th list: 7 -> 8
+// The merged list will be:
+ 
+// Input: arr[] = [1 -> 3, 8, 4 -> 5 -> 6]
+// Output: 1 -> 3 -> 4 -> 5 -> 6 -> 8
+// Explanation:
+// The arr[] has 3 sorted linked list of size 2, 3, 1.
+// 1st list: 1 -> 3
+// 2nd list: 8
+// 3rd list: 4 -> 5 -> 6
+// The merged list will be:
+
+// Constraints
+// 1 <= total no. of nodes <= 105
+// 1 <= node->data <= 103
+
+// Company Tags
+// VMWareAmazonMicrosoftOracle
+// Topic Tags
+// Related Interview Experiences
+// Related Articles
+// Expected Complexities
+// If you ar
+
+
+class Solution {
+    public:
+  struct compare {
+      bool operator()(Node* a, Node* b) {
+          return a->data > b->data; // Min-heap based on node values
+      }
+  };
+      Node* mergeKLists(vector<Node*>& arr) {
+          // Your code here
+          priority_queue<Node*,vector<Node*>,compare>minH;
+          for(auto list:arr){
+              if(list)minH.push(list);
+          }
+          Node* dummy=new Node(0);
+          Node* tail=dummy;
+          while(!minH.empty()){
+              Node* small=minH.top();
+              minH.pop();
+              tail->next=small;
+              tail=tail->next;
+              if(small->next){
+                  minH.push(small->next);
+              }
+          }
+          return dummy->next;
+      }
+  };
+  
