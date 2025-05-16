f3ca3d70d6fd00977c887c0c64b3b5c6fbfe0f82
Sumant <sumantyadav3086@gmail.com>
2025-05-16 13:48:17 +0530

    mp

---
+++ Smallest range in K lists.cpp
@@ -0,0 +1,54 @@
+// Smallest range in K lists
+// Difficulty: HardAccuracy: 43.21%Submissions: 32K+Points: 8Average Time: 40m
+// Given a 2d integer array arr[][] of size k*n, where each row is sorted in ascending order. Your task is to find the smallest range [l, r] that includes at least one element from each of the k lists. If more than one such ranges are found, return the first one.
+
+// Examples:
+
+// Input: n = 5, k = 3, arr[][] = [[4, 7, 9, 12, 15], [0, 8, 10, 14, 20], [6, 12, 16, 30, 50]]
+// Output: [6, 8]
+// Explanation: Smallest range is formed by  number 7 from the first list, 8 from second list and 6 from the third list.
+// Input: n = 5, k = 3, arr[][] = [[1, 3, 5, 7, 9], [0, 2, 4, 6, 8], [2, 3, 5, 7, 11]]
+// Output: [1, 2]
+// Explanation: Smallest range is formed by number 1 present in first list and 2 is present in both 2nd and 3rd list.
+// Input: n = 2, k = 3, arr[][] = [[2, 4], [1, 7], [20, 40]]
+// Output: [4, 20]
+// Explanation: Smallest range is formed by number 4 from the first list, 7 from second list and 20 from the third list.
+// Constraints:
+// 1 <= k, n <= 500
+// 0 <= arr[ i ] <= 105
+// Expected Complexities
+// Company Tags
+// FlipkartAmazon
+// Topic Tags
+// Related Interview Experiences
+// Related Articles
+// If you are facing any issue on this page. Please let us know.
+  vector<int> findSmallestRange(vector<vector<int>>& arr) {
+        // code here
+          map<int,int>poin;
+        int maxi=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minH;
+        int minRange=INT_MAX;
+        for(int i=0;i<arr.size();i++){
+            poin[i]=0;
+            minH.push({arr[i][0],i});
+            maxi=max(maxi,arr[i][0]);
+        }
+        vector<int>ans;
+        while(true){
+            auto ele=minH.top();
+            int range=maxi-ele.first;
+            minH.pop();
+            if(range<minRange){
+                minRange=range;
+                ans={ele.first,maxi};
+            }
+            poin[ele.second]++;
+             if (poin[ele.second] == arr[ele.second].size()) {
+            break;  
+        }
+            minH.push({arr[ele.second][poin[ele.second]],ele.second});
+            maxi=max(maxi,arr[ele.second][poin[ele.second]]);
+        }
+        return ans;
+    }
